d4dc11f4bf268ca02564404363ee581f0ba31e81
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-11-12 20:10:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-window.promises = [];
+window.promises = [
+  new Promise((resolve) => setTimeout(() => resolve("Promise 1 resolved"), getRandomTime())),
+  new Promise((resolve) => setTimeout(() => resolve("Promise 2 resolved"), getRandomTime())),
+  new Promise((resolve) => setTimeout(() => resolve("Promise 3 resolved"), getRandomTime())),
+  new Promise((resolve) => setTimeout(() => resolve("Promise 4 resolved"), getRandomTime())),
+  new Promise((resolve) => setTimeout(() => resolve("Promise 5 resolved"), getRandomTime()))
+];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+function getRandomTime() {
+  return Math.floor(Math.random() * 5000) + 1000; // Random time between 1000 and 5000 milliseconds (1 and 5 seconds)
+}
+
+Promise.any(window.promises)
+  .then((result) => {
+    // Print the result to the div with id "output"
+    document.getElementById("output").innerText = result;
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
